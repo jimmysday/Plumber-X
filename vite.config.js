6c40e554c66c8f470e9dd42d56a740606c77656f
jimmy <jaminzhang99@gmail.com>
2025-03-09 00:01:16 -0800

    publish websit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "dist/index.html", dest: "dist/404.html" }],
+    }),
+  ],
+  base: "/Plumber-X/", // 重要！GitHub Pages 需要设置 base 路径
+});
